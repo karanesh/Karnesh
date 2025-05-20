9765fdc0478046663dfe9d6a5d38fe88b48a5d97
karanesh <karneshwaran603@gmail.com>
2025-05-20 19:38:45 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,129 +1,100 @@
-from keras import backend as K
-from tensorflow.keras.models import Model, load_model
+# Module 1: Import necessary packages
 import streamlit as st
-from nltk.tokenize import word_tokenize
-from tensorflow.keras.preprocessing.sequence import pad_sequences
-import pickle
-import re
-import string
-from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
-import pandas as pd
 import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer, CountVectorizer
+from sklearn.svm import LinearSVC
+from sklearn.naive_bayes import MultinomialNB
+import warnings
+import streamlit_lottie
+warnings.filterwarnings("ignore")
 
-import plotly.express as px
-import plotly.graph_objects as go
+# Module 2: Load the dataset
+@st.cache
+def load_data():
+    data = pd.read_csv("fake_or_real_news.csv")
+    data['fake'] = data['label'].apply(lambda x: 0 if x == 'REAL' else 1)
+    return data
 
-MODEL_PATH = r"models/LSTM_model_1.h5"
-MAX_NB_WORDS = 100000    # max no. of words for tokenizer
-MAX_SEQUENCE_LENGTH = 200 # max length of each entry (sentence), including padding
-VALIDATION_SPLIT = 0.2   # data for validation (not used in training)
-EMBEDDING_DIM = 100
-tokenizer_file = "tokenizer.pickle"
+# Module 3: Select Vectorizer and Classifier
+def select_model():
+    vectorizer_type = st.sidebar.selectbox("Select Vectorizer", ["TF-IDF", "Bag of Words"])
+    classifier_type = st.sidebar.selectbox("Select Classifier", ["Linear SVM", "Naive Bayes"])
+    
+    vectorizer = None
+    if vectorizer_type == "TF-IDF":
+        vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+    elif vectorizer_type == "Bag of Words":
+        vectorizer = CountVectorizer(stop_words='english', max_df=0.7)
+    
+    classifier = None
+    if classifier_type == "Linear SVM":
+        classifier = LinearSVC()
+    elif classifier_type == "Naive Bayes":
+        classifier = MultinomialNB()
+    
+    return vectorizer, classifier
 
-wordnet = WordNetLemmatizer()
-regex = re.compile('[%s]' % re.escape(string.punctuation))
+# Module 4: Train the model
+@st.cache
+def train_model(data, vectorizer, classifier):
+    x_vectorized = vectorizer.fit_transform(data['text'])
+    clf = classifier.fit(x_vectorized, data['fake'])
+    return clf
 
-model_list = ["Logistic Regression",'Multinomial Naive Bayes Classifier','Bernoulli Naive Bayes Classifier','Gradient Boost Classifier','Decision Tree','RFC Classifier']
-model_file_list = [r"models/LR_model.pkl",r"models/MNVBC_model.pkl",r"models/BNBC_model.pkl",r"models/GBC_model.pkl",r"models/DT_model.pkl",r"models/RFC_model.pkl"]
+# Module 5: Streamlit app
+def main():
+    # Set page configuration
+    page_icon = ":metro:"  # emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
+    layout = "wide"
+    page_title = "Fake News Detection"
+    st.set_page_config(page_title=page_title, page_icon=page_icon, layout=layout)
+    
+    # Streamlit app
+    st.title(page_title + " " + page_icon)
+    st.lottie("https://lottie.host/bd0c4818-c5a6-4e42-b407-746bc448c2c7/ipVUdgFncO.json", width=200, height=200)
 
-with open(tokenizer_file, 'rb') as handle:
-    tokenizer = pickle.load(handle)
+    # --- HIDE STREAMLIT STYLE ---
+    hide_st_style = """
+    <style>
+    #MainMenu {visibility: hidden;}
+    footer {visibility: hidden;}
+    header {visibility: hidden;}
+    </style>
+    """
+    st.markdown(hide_st_style, unsafe_allow_html=True)
 
-def basic_text_cleaning(line_from_column):
-    # This function takes in a string, not a list or an array for the arg line_from_column
-
-    tokenized_doc = word_tokenize(line_from_column)
-
-    new_review = []
-    for token in tokenized_doc:
-        new_token = regex.sub(u'', token)
-        if not new_token == u'':
-            new_review.append(new_token)
-
-    new_term_vector = []
-    for word in new_review:
-        if not word in stopwords.words('english'):
-            new_term_vector.append(word)
-
-    final_doc = []
-    for word in new_term_vector:
-        final_doc.append(wordnet.lemmatize(word))
-
-    return ' '.join(final_doc)
-
-@st.cache(allow_output_mutation=True)
-def Load_model():
-    model = load_model(MODEL_PATH)
-    model._make_predict_function()
-    model.summary()  # included to make it visible when model is reloaded
-    session = K.get_session()
-    return model, session
-
-if _name_ == '_main_':
-    st.title('Fake News Classification app ')
-    st.write("A simple fake news classification app utilising 6 traditional ML classifiers and a LSTM model")
-    st.info("LSTM model, tokeniser and the 6 traditional machine learning models loaded ")
-    st.subheader("Input the News content below")
-    sentence = st.text_area("Enter your news content here", "Some news",height=200)
-    predict_btt = st.button("predict")
-    model, session = Load_model()
-    if predict_btt:
-        clean_text = []
-        K.set_session(session)
-        i = basic_text_cleaning(sentence)
-        clean_text.append(i)
-        sequences = tokenizer.texts_to_sequences(clean_text)
-        data = pad_sequences(sequences, padding = 'post', maxlen = MAX_SEQUENCE_LENGTH)
-        prediction = model.predict(data)
-
-        prediction_prob_real = prediction[0][0]
-        prediction_prob_fake = prediction[0][1]
-        prediction_class = prediction.argmax(axis=-1)[0]
-
-        st.header("Prediction using LSTM model")
-
-        if prediction_class == 0:
-            st.success('This is not a fake news')
-        if prediction_class == 1:
-            st.warning('This is a fake news')
-
-        class_label = ["fake","true"]
-        prob_list = [prediction[0][1]*100,prediction[0][0]*100]
-        prob_dict = {"true/fake":class_label,"Probability":prob_list}
-        df_prob = pd.DataFrame(prob_dict)
-        fig = px.bar(df_prob, x='true/fake', y='Probability')
-        model_option = "LSTM"
-        if prediction[0][1] > 0.7:
-            fig.update_layout(title_text="{} model - prediction probability comparison between true and fake".format(model_option))
-            st.info("The {} model predicts that there is a higher {} probability that the news content is fake compared to a {} probability of being true".format(model_option,prediction[0][1]*100,prediction[0][0]*100))
-        elif prediction[0][0] > 0.7:
-            fig.update_layout(title_text="{} model - prediction probability comparison between true and fake".format(model_option))
-            st.info("The {} model predicts that there is a higher {} probability that the news content is true compared to a {} probability of being fake".format(model_option,prediction[0][0]*100,prediction[0][1]*100))
+    # Load data
+    data = load_data()
+    
+    # Select vectorizer and classifier
+    vectorizer, classifier = select_model()
+    
+    # Text input for user to input news article
+    user_input = st.text_area("Enter your news article here:")
+    
+    # When user submits the input
+    if st.button("Check"):
+        # Train the model
+        clf = train_model(data, vectorizer, classifier)
+        
+        # Vectorize the user input
+        input_vectorized = vectorizer.transform([user_input])
+        
+        # Predict the label of the input
+        prediction = clf.predict(input_vectorized)
+        
+        # Convert prediction to integer for interpretation
+        result = int(prediction[0])
+        
+        # Display the result
+        if result == 1:
+            st.error("This news article is likely fake!")
         else:
-            fig.update_layout(title_text="{} model - prediction probability comparison between true and fake".format(model_option))
-            st.info("Your news content is rather abstract, The {} model predicts that there a almost equal {} probability that the news content is true compared to a {} probability of being fake".format(model_option,prediction[0][0]*100,prediction[0][1]*100))
-        st.plotly_chart(fig, use_container_width=True)
+            st.success("This news article seems to be real.")
 
-        st.header("Prediction using 6 traditional machine learning model")
-        predictions = []
-        for model in model_file_list:
-            filename = model
-            model = pickle.load(open(filename, "rb"))
-            prediction = model.predict([sentence])[0]
-            predictions.append(prediction)
-
-        dict_prediction = {"Models":model_list,"predictions":predictions}
-        df = pd.DataFrame(dict_prediction)
-
-        num_values = df["predictions"].value_counts().tolist()
-        num_labels = df["predictions"].value_counts().keys().tolist()
-
-        dict_values = {"true/fake":num_labels,"values":num_values}
-        df_prediction = pd.DataFrame(dict_values)
-        fig = px.pie(df_prediction, values='values', names='true/fake')
-        fig.update_layout(title_text="Comparision between all 7 models: Prediction proportion between True/Fake")
-        st.plotly_chart(fig, use_container_width=True)
-        st.table(df)
+# Run the Streamlit app
+if _name_ == "_main_":
+    main()
+st.markdown("*Created with enthusiasm by SuperSam*")
+##run with command streamlit run main.py --client.showErrorDetails=false to remove cache error message on streamlit interface
